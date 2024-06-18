1ab3f525329bb280a4b216de781f7fc2144aee52
Dhadage <ramnathbalasaheb.dhadage@bms.com>
2024-06-18 15:26:56 +0530

    added Rtable Example

---
+++ R/custom_data_block.R
@@ -0,0 +1,32 @@
+#' @import blockr falcon rtables
+new_custom_data_block<- function(...){
+  blockr::new_block(
+    name = "boxplot scatter data",
+    expr = quote({
+      # n <- 350 # Sample size
+      # BiomarkerX <- rnorm(n, mean = 50, sd = 20)
+      # BiomarkerY <- 0.7 * BiomarkerX + rnorm(n, mean = 0, sd = 15)
+      # data.frame(BiomarkerX, BiomarkerY)
+      adsl <- random.cdisc.data::cadsl
+      data <- falcon::make_table_02(
+        df = adsl,
+        vars = c("SEX", "AGE", "RACE"),
+        lbl_vars = c("Sex", "Age, years", "Race")
+      ) |> rtables::as_result_df()
+    }),
+    fields = list(),
+    class = c("transform_block")
+  )
+}
+
+#' @param data Result from previous block
+#' @rdname evaluate_block
+#' @export
+evaluate_block.transform_block <- function(x, data, ...) {
+  stopifnot(...length() == 0L)
+  browser()
+  eval(
+    substitute(data %>% expr, list(expr = generate_code(x))),
+    list(data = data)
+  )
+}
--- app.R
+++ app.R
@@ -2,37 +2,22 @@ devtools::document()
 pkgload::load_all()
 library(shiny)
 library(blockr)
-library(blockr.bms)
+# library(blockr.bms)
 library(blockr.pharmaverseadam)
 
-new_two_way_data_block <- function(...){
-  blockr::new_block(
-    name = "boxplot scatter data",
-    expr = quote({
-      n <- 100
-      data <- data.frame(
-        Subgroup_1 = factor(sample(c("Low", "Medium", "High"), n, replace = TRUE)),
-        Subgroup_2 = factor(sample(c("A", "B", "C"), n, replace = TRUE)),
-        Value = c(rnorm(n, mean = 10, sd = 15), rnorm(n, mean = 20, sd = 45))
-      )
-    }),
-    fields = list(),
-    class = c("two_way_data", "data_block")
-  )
-}
 stack1 <- new_stack(
-  new_two_way_data_block,
-  two_way_summary_table_block,
-  title = "two way summary"
+  # adam_block,
+  new_custom_data_block
+  # my_block
 )
+serve_stack(stack1)
+# ui <- fluidPage(
+#   theme = bslib::bs_theme(5L),
+#   generate_ui(stack1)
+# )
+# 
+# server <- function(input, output, session){
+#   generate_server(stack1)
+# }
 
-ui <- fluidPage(
-  theme = bslib::bs_theme(5L),
-  generate_ui(stack1)
-)
-
-server <- function(input, output, session){
-  generate_server(stack1)
-}
-
-shinyApp(ui, server)
+# shinyApp(ui, server)
